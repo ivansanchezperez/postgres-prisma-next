fd120a37e650443dba7d79ac646986b5d4b60359
ivansanchezperez <ivan@DESKTOP-C5UAKBC>
2023-10-28 20:13:26 +0200

    ✨: starting ui

---
+++ app/dashboard/page.tsx
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+  return (
+    <main className="relative flex min-h-screen flex-col items-center justify-center">
+      Dashboard
+    </main>
+  );
+};
+
+export default Dashboard;
